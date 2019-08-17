c160fc159c568c3dab716434d21898fdf0c1e571
Will Harris <will@lifenet.net>
2019-08-17 16:12:34 -0700

    setting up express

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const http = require("http");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
+});
 
-const server = http.createServer(routes.handler);
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3000);

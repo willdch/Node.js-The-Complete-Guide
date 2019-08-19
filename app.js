bf9771abcda21de5d9136612b9a57c31a7884c29
Will Harris <will@lifenet.net>
2019-08-19 14:58:29 -0700

    added hbs and ejs templates

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
 
+//when using the handlebars template you must register the enigne like below.
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs"
+  })
+);
 
 // ⬇︎ is telling express we want to use pug(jade) for dynamic templating.  Not all dynamic templates come pre set up with express but pug is.
 // app.set('view engine', 'pug');
+app.set("view engine", "hbs");
+// app.set("view engine", "ejs");
 // ⬇︎ for pug this is not needed but it is for temples that are not inately part of express this would tell express what folders we want rendering the vies.  along with the above import of the template.
-app.set('views', 'views');
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // ⬇︎ serves the public file statically to allow for use of css style sheets and can be used for many other things.
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle: 'Page not Found', pageTitle: 'Page not Found'});
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res
+    .status(404)
+    .render("404", {
+      pageTitle: "Page not Found",
+      pageTitle: "Page not Found"
+    });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
-
 // admin/add=product => GET
 router.get("/add-product", (req, res, next) => {
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', {pageTitle: 'Add product', path: '/admin/add-product'});
-    });
-    
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product", {
+    pageTitle: "Add product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+});
+
 // admin/add=product => POST
 router.post("/add-product", (req, res, next) => {
-  products.push({title: req.body.title});
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
